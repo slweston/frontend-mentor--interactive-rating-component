249b6c8ca41529fb41927a11c36169f09ee57f47
Scott Weston <swest2020-github@yahoo.com>
2022-11-22 16:03:45 -0600

    initial commit

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+(() => {
+	const form = document.querySelector('.card__form');
+	form.addEventListener('submit', (e) => {
+		e.preventDefault();
+		const ratingNumber = form.elements['ratingNumber'].value;
+		const ratingYouSelectedElement = document.querySelector('.card__rating-you-selected');
+		ratingYouSelectedElement.innerText = 'You selected ' + ratingNumber + ' out of 5';
+
+		const ratingCard = document.querySelector('.card--rating-state');
+		const thankYouCard = document.querySelector('.card--thank-you-state');
+
+		ratingCard.style.display = 'none';
+		thankYouCard.style.display = 'grid';
+	});
+})();
